e689cb893784c2b03fb39a716a724fd256b4a3c6
Denis Medved <thebucefal@gmail.com>
2011-10-19 00:43:00 +0300

    white delegator. (not finished)

--- src/audioitemdelegate.cpp
+++ src/audioitemdelegate.cpp
@@ -1,7 +1,8 @@
 
 #include <QDebug>
 #include <QPainter>
-
+#include <QTextLayout>
+#include <QTextLine>
 #include "audioitemdelegate.h"
 
 AudioItemDelegate::AudioItemDelegate(QObject *parent) :
@@ -11,14 +12,57 @@ AudioItemDelegate::AudioItemDelegate(QObject *parent) :
 
 void AudioItemDelegate::paint ( QPainter * painter, const QStyleOptionViewItem & option, const QModelIndex & index ) const
 {
-	qDebug() << index.data(1).toString();
-	painter->drawText(option.rect,0,index.data(1).toString());
+	int x = option.rect.x() -1 ;
+	int y = option.rect.y() -1;
+	int w = option.rect.width() -1;
+	int h = option.rect.height()-1;
 
-	//( const QRect & rectangle, int flags, const QString & text, QRect * boundingRect = 0 )
+	QFont font = option.font;
+	font.setPixelSize(11);
+	font.setFamily("	tahoma,arial,verdana,sans-serif,Lucida Sans");
+	font.setBold(true);
+	QFontMetrics  fm = QFontMetrics(font);
+
+	QTextLayout artist;
+	artist.setFont(font);
+	artist.setText(index.data(1).toString());
+	QPen pen(Qt::SolidLine);
+	pen.setColor(QColor("#2B587A"));
+	painter->setPen(pen);
+	artist.beginLayout();
+	int leading = fm.leading();
+	qreal height = 0;
+	int lineWidth = w - 50;
+	while (1)
+	{
+	    QTextLine line = artist.createLine();
+	    if (!line.isValid())
+		break;
+
+	    line.setLineWidth(lineWidth);
+	    height += leading;
+	    line.setPosition(QPointF(0, y+height));
+	    height += line.height();
+	}
+
+	artist.endLayout();
+	artist.draw(painter,QPoint(15,10));
+
+
+	pen.setStyle(Qt::DashLine);
+	painter->setPen(pen);
+	painter->drawLine(x ,y+h,x + w,y + h);
+	/*
+	painter->drawLine(option.rect.x(),option.rect.y(), option.rect.x() + option.rect.width(),option.rect.y() + option.rect.height());
+	painter->drawLine(option.rect.x() + option.rect.width(),option.rect.y() , option.rect.x(),option.rect.y() + option.rect.height());
+	*/
 }
 
 
 QSize AudioItemDelegate::sizeHint ( const QStyleOptionViewItem & option, const QModelIndex & index ) const
 {
-	return QSize(100,20);
+	if (!index.isValid())
+		return QSize();
+
+	return QSize(option.rect.width(),40);
 }
